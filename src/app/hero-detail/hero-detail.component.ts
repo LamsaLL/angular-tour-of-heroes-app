13818dca4bcf730a1ed6de83570e3054cedc0003
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-02-15 14:25:43 +0100

    refactor(hero-detail): add hero detail component

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../data/hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  @Input() hero?: Hero;
+  constructor() {}
+  ngOnInit(): void {}
 }
