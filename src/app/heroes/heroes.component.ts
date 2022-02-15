7647c99a2e90db68596c12318ad3e71289587d4a
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-02-15 14:38:40 +0100

    feat(hero): add hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../data/hero';
-import { HEROES } from '../mock-heroes';
-
+import { HEROES } from '../data/mock-heroes';
+import { HeroService } from '../service/hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
   selectedHero?: Hero;
 
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
