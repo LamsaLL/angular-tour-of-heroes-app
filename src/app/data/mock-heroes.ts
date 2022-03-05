0ccb8b746af34a5e3a63a96864640618e909e44f
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-03-05 17:40:17 +0100

    feat(data/hero): add attributes for heroes

--- src/app/data/mock-heroes.ts
+++ src/app/data/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
+  { id: 11, name: 'Dr Nice', attack: 2, dodge: 2, lp: 5 },
+  { id: 12, name: 'Narco', attack: 2, dodge: 2, lp: 5 },
+  { id: 13, name: 'Bombasto', attack: 2, dodge: 2, lp: 5 },
+  { id: 14, name: 'Celeritas', attack: 2, dodge: 2, lp: 5 },
+  { id: 15, name: 'Magneta', attack: 2, dodge: 2, lp: 5 },
+  { id: 16, name: 'RubberMan', attack: 2, dodge: 2, lp: 5 },
+  { id: 17, name: 'Dynama', attack: 2, dodge: 2, lp: 5 },
+  { id: 18, name: 'Dr IQ', attack: 2, dodge: 2, lp: 5 },
+  { id: 19, name: 'Magma', attack: 2, dodge: 2, lp: 5 },
+  { id: 20, name: 'Tornado', attack: 2, dodge: 2, lp: 5 },
 ];
