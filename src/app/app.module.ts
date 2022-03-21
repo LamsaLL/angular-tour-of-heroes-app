d8cf355c0a10782b31708bd199b0eaaf551a6f97
Léo Amsallem <leo.amsallem@etu.univ-grenoble-alpes.fr>
2022-03-21 13:19:03 +0100

    feat: get hero with firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessagesComponent } from './messages/messages.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,15 +23,15 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    // provideFirestore(() => getFirestore()),
+    //provideFirebaseApp(() => initializeApp(environment.firebase)),
+    //provideFirestore(() => getFirestore())
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/data/hero.ts
+++ src/app/data/hero.ts
@@ -1,7 +1,17 @@
-export interface Hero {
-  id: number;
-  name: string;
-  attack: number;
-  dodge: number;
-  lp: number;
+import { Serializable } from './serializable';
+
+export class Hero extends Serializable {
+  id?: string;
+  name?: string;
+  attack?: number;
+  dodge?: number;
+  lp?: number;
+
+  constructor() {
+    super();
+  }
+
+  uneMethode(): string {
+    return 'le nom de mon hero' + this.name;
+  }
 }
